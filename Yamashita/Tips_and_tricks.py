d846817f75afbb6385f930246715cb63149ba678
tomyamashita <46978822+tomyamashita@users.noreply.github.com>
2019-02-09 13:44:54 -0600

    Add files via upload

---
+++ Yamashita/Tips_and_tricks.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 30 18:18:41 2019
+
+@author: tomyamashita
+"""
+
+"""
+Spyder Tips and tricks
+by Tom Yamashita
+"""
+
+# Running Code: 
+"""
+Use the #%% symbol at the beginning of a line to create new cells in the script
+
+To run a single line of code, use: F9
+
+To run a selection of code: 
+    1. highlight code you want to run
+    2. F9
+
+To run an entire cell, use:
+    ctrl+enter (does not go to next cell)
+    shift+enter (goes to next cell)
+
+"""
+
+# Changing the color scheme
+"""
+Can change the color scheme for spyder by: 
+    1. Go to Preferences
+    2. Syntax coloring
+    3. Edit selected
+    4. Change to whatever colors you want
+
+"""
+
+# Spyder interface
+"""
+You can load or save files from any location
+
+Windows can be rearranged and resized depending on personal preferences
+
+To use the built in help, have cursor on a function -> ctrl+I
+This brings up the same thing as using the ? at the end of a function except it opens in the help window instead of the console
+
+Can use the variable explorer to view variables including the values they refer to and their type
+    You can also clear all variables using the eraser looking thing in the variable explorer and console windows
+
+Can set a working directory which is the place where things are saved using the file explorer and bar on the top of the page
+
+Can look at things that were done in previous sessions by looking at the history log
+
+"""
+
+# Matplotlib
+"""
+You can use the same function as for Jupyter Notebooks to embed plots in the console:
+    %matplotlib inline
+Otherwise plots open in a separate window which you can then edit for export
+Plots can be exported directly from the console but they cannot be edited
+
+"""
